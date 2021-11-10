b20a227332952dd1eab98cc778f4f615821cd5ad
Giulio Luongo <giulio.luongo@outlook.it>
2021-11-10 18:22:48 +0100

    Cmakelists create correttamente

---
+++ CustomPrograms/CMakeFiles/TorqueControlGeneric.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TorqueControlGeneric.dir/TorqueControlGeneric.cpp.o"
+  "TorqueControlGeneric"
+  "TorqueControlGeneric.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TorqueControlGeneric.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CustomPrograms/CMakeFiles/TorqueControlGeneric.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TorqueControlGeneric.
+# This may be replaced when dependencies are built.
